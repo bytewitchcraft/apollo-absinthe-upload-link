8cadc2165cbde3ab57128ad907b515b2c06ce576
Leandro Cesquini Pereira <leandro.cesquini@gmail.com>
2018-06-18 16:56:51 -0300

    Consider Blob as a valid upload file

--- src/validators.js
+++ src/validators.js
@@ -1,9 +1,11 @@
 export const isObject = value => value !== null && typeof value === 'object'
+
 export const isFileList = value =>
   typeof FileList !== 'undefined' && value instanceof FileList
 
 export const isUploadFile = value =>
   (typeof File !== 'undefined' && value instanceof File) ||
+  (typeof Blob !== 'undefined' && value instanceof Blob) ||
   value instanceof ReactNativeFile
 
 /**
